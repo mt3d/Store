a61aa08678ebaad3943f1142e57b21bfc168e4c7
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-07-26 10:12:17 +0300

    Create a storage-aware cart class

--- OnlineStore/Models/Cart.cs
+++ OnlineStore/Models/Cart.cs
@@ -4,7 +4,7 @@
 	{
 		public List<CartLine> Lines { get; set; } = new List<CartLine>();
 
-		public void AddItem(Product product, int quantity)
+		public virtual void AddItem(Product product, int quantity)
 		{
 			CartLine? line = Lines.Where(p => p.Product.ProductId == product.ProductId).FirstOrDefault();
 
@@ -17,14 +17,14 @@
 			}
 		}
 
-		public void RemoveLine(Product product)
+		public virtual void RemoveLine(Product product)
 		{
 			Lines.RemoveAll(l => l.Product.ProductId == product.ProductId);
 		}
 
 		public decimal ComputeTotalValues() => Lines.Sum(e => e.Product.Price * e.Quantity);
 
-		public void Clear() => Lines.Clear();
+		public virtual void Clear() => Lines.Clear();
 	}
 
 	public class CartLine
