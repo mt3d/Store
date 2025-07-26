476af415180a35df3c609bb95e3e332053d1e2bc
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-07-26 22:49:02 +0300

    Add control and view for the checkout process

---
+++ OnlineStore/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Models;
+
+namespace OnlineStore.Controllers
+{
+	public class OrderController : Controller
+	{
+		public ViewResult Checkout() => View(new Order());
+	}
+}
