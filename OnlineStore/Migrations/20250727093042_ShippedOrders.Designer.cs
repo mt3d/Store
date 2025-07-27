e7095775ff01916774e72f2bebe6ecd455a1ce85
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-07-27 18:39:16 +0300

    Display orders to the adminstrator

--- OnlineStore/Migrations/20250727093042_ShippedOrders.Designer.cs
+++ OnlineStore/Migrations/20250727093042_ShippedOrders.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineStore.Models;
 
@@ -11,9 +12,11 @@ using OnlineStore.Models;
 namespace OnlineStore.Migrations
 {
     [DbContext(typeof(StoreDbContext))]
-    partial class StoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250727093042_ShippedOrders")]
+    partial class ShippedOrders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
