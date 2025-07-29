a76dba232d7974a1c966bda3bf8488faee874563
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-07-29 06:50:36 +0300

    Configure error handling

---
+++ OnlineStore/Pages/Error.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineStore.Pages
+{
+    public class ErrorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
